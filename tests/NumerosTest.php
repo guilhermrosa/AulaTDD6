861d041293cd01bf8a92aed622e809c2e74880e3
Thiago Durso <73541189+thiagodurso@users.noreply.github.com>
2021-10-28 16:36:27 -0300

    Adicionei 2 novos testes

--- tests/NumerosTest.php
+++ tests/NumerosTest.php
@@ -32,7 +32,32 @@ final class NumerosTest extends TestCase{
 		
 
 	 	$this->assertEquals($array->menor(), 1);
+	 }
 
+	 	public function TestProduto(){
+
+		$array = new Numeros();
+		$array->inserir(1);
+		$array->inserir(2);
+		$array->inserir(3);
+		$array->inserir(3);
+
+		$this->assertEquals($array->produto(),18);
+
+	}
+	public function TestContar(){
+
+		$array = new Numeros();
+		$array->inserir(1);
+		$array->inserir(2);
+		$array->inserir(3);
+		$array->inserir(3);
+
+
+		$this->assertEquals($array->contar(3), 2);
+
+							
+	}
 
 			
 							
